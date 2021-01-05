2afe6ea924326e6a92b2dae48482e30b5ef17d63
Julian-Murillo <jomsk822@gmail.com>
2021-01-05 18:25:45 -0500

    updates capability component and retrieves capabilities from server

--- src/components/CapabilityTool/Capability.js
+++ src/components/CapabilityTool/Capability.js
@@ -1,9 +1,38 @@
-import React from "react";
+  
+import React, { Component } from "react";
 
-const Capability = () => {
+export class Capability extends Component {
+  render() {
+    const {
+        id,
+        numOfAvailableDevelopers,
+        numOfDevelopers,
+        techStack,
+        _links
+    } = this.props.capability;
+
     return (
-        <h1>Capability</h1>
-    )
-};
+      <div className="card card-body border-primary mb-3">
+        <h4 className="text-primary">
+          {techStack} ...{_links.getThisCapability.href}
+          <i className="fas fa-user-edit ml-2" style={{ color: "blue" }} />
+          <i className="fas fa-user-times ml-2" style={{ color: "red" }} />
+        </h4>
+
+        <ul className="list-group">
+          <li className="list-group-item bg-light text-primary">
+            Technology Stack: {techStack}
+          </li>
+          <li className="list-group-item bg-light  text-primary">
+            Total Developers in Capability: {numOfDevelopers}
+          </li>
+          <li className="list-group-item bg-light  text-primary">
+            Available developers for hire: {numOfAvailableDevelopers}
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Capability;
