67e1ca9d9c01c58d4bb8508f17fbd891c631a016
Julian-Murillo <jomsk822@gmail.com>
2021-01-05 17:51:51 -0500

    populates layout components

--- src/components/Layout/NotFound.js
+++ src/components/Layout/NotFound.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const NotFound = () => {
-
-    return (
-        <h1>NotFound</h1>
-    )
+function NotFound() {
+  return (
+    <div>
+      <h1 className="display-4 text-center">404 Page not found</h1>
+      <h2 className="display-4 text-center">What are you looking for?</h2>
+    </div>
+  );
 }
 
 export default NotFound;
