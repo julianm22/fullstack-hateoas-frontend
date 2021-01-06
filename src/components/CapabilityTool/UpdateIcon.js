9f754a53db9f3124c731352edf6d3f78419b0316
Julian-Murillo <jomsk822@gmail.com>
2021-01-06 18:05:48 -0500

    abstracts update icon from capability to own component and sets up modals for add cap btn and updateicon

---
+++ src/components/CapabilityTool/UpdateIcon.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from 'react'
+import Modal from "react-modal";
+import UpdateCapability from "./UpdateCapability";
+
+const customStyles = {
+    content: {
+        height: "500px"
+    }
+};
+
+class UpdateIcon extends Component {
+
+    state = {
+        modalIsOpen: false
+    };
+
+    openModal = () => {
+        this.setState({ modalIsOpen: true });
+    };
+
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <i className="fas fa-user-edit ml-2" style={{ color: "blue" }} onClick={this.openModal} />
+                <Modal isOpen={this.state.modalIsOpen} style={customStyles} >
+                    <button 
+                        type="button"
+                        className="btn btn-danger mb-2"
+                        onClick={this.closeModal}
+                    >
+                        <i className="far fa-times-circle mr-1" /> Close Modal
+                    </button>
+                    <UpdateCapability />
+                </Modal>
+            </Fragment>
+        );
+    }
+}
+
+export default UpdateIcon
